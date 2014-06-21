589040892a838bf3720d6ab433be132d8cc53650
Erwin Oldenkamp <oldenkamperwin@gmail.com>
2014-06-21 23:30:57 +0200

    add some bootstrap directives and modules

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,12 @@
 'use strict';
-angular.module('jbehaveWebApp', ['ngRoute']).config(function($routeProvider) {
+angular.module('jbehaveWebApp', ['ngRoute', 'mgcrea.ngStrap', 'ui.bootstrap']).config(function($routeProvider) {
   return $routeProvider.when('/', {
+    templateUrl: 'views/main.html',
+    controller: 'StoryCtrl'
+  }).when('/build', {
+    templateUrl: 'views/main.html',
+    controller: 'StoryCtrl'
+  }).when('/configure', {
     templateUrl: 'views/main.html',
     controller: 'StoryCtrl'
   }).otherwise({
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,18 +2,27 @@
 angular.module('jbehaveWebApp').controller('StoryCtrl', ['$scope', '$http', 'filterFilter',
   function StoryCtrl($scope, $http) {
     $scope.stories = [];
-    $scope.checkedStories = [];
 
     $scope.loadStories = function() {
       $http({
         method: 'GET',
-        url: 'http://localhost:8080/api/stories'
+        url: 'http://192.168.178.15:8080/api/stories'
       }).
       success(function(data) {
-        console.log(data);
-        var storiesResult = data;
         $scope.stories = data;
       });
     };
+
+    $scope.runStory = function($storyid){
+      console.log('this will run story  '+$storyid)
+      $http({
+        method: 'POST',
+        url: "http://192.168.178.15:8080/api/runner/story",
+        data: $storyid
+      }).
+      success(function(data){
+        console.log('status id = ' +data)
+      });
+    }
   }
 ]);
