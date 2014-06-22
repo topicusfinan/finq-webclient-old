bc763995c056d1c6b029ac908ffbbb3e56e38940
Erwin Oldenkamp <oldenkamperwin@gmail.com>
2014-06-22 22:25:18 +0200

    change to localhost

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,7 +6,7 @@ angular.module('jbehaveWebApp').controller('StoryCtrl', ['$scope', '$http', 'fil
     $scope.loadStories = function() {
       $http({
         method: 'GET',
-        url: 'http://192.168.178.15:8080/api/stories'
+        url: 'http://localhost:8080/api/stories'
       }).
       success(function(data) {
         $scope.stories = data;
@@ -17,7 +17,7 @@ angular.module('jbehaveWebApp').controller('StoryCtrl', ['$scope', '$http', 'fil
       console.log('this will run story  '+$storyid)
       $http({
         method: 'POST',
-        url: "http://192.168.178.15:8080/api/runner/story",
+        url: "http://localhost:8080/api/runner/story",
         data: $storyid
       }).
       success(function(data){
