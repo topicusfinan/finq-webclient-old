b572670e34d91711cdb9b9ebd88f6626f6998ccc
sgleeffers <sgleeffers@gmail.com>
2014-07-07 09:02:24 +0200

    Front-end improvements

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,37 +2,49 @@
 angular.module('jbehaveWebApp').controller('StoryCtrl', ['$scope', '$http', 'filterFilter',
   function StoryCtrl($scope, $http) {
     $scope.stories = [];
+    $scope.scenarios = [];
 
-    $scope.loadStories = function() {
+    $scope.scenarioFilter = [];
+
       $http({
         method: 'GET',
-        url: 'http://192.168.42.44:8080/api/stories'
+        url: 'http://192.168.42.65:8080/api/stories'
       }).
       success(function(data) {
         $scope.stories = data;
+        console.log(data);
       });
+
+
+    $scope.setFilter = function() { 
+      $scope.scenarioFilter.push(this.id);
+    };
+    
+
+    $scope.loadScenarios = function($index) { 
+      $scope.scenarios = $scope.stories[$index].scenarios;
     };
 
     $scope.runStory = function($storyid){
 
       $http({
         method: 'POST',
-        url: "http://192.168.42.44:8080/api/runner/story",
+        url: 'http://192.168.42.65:8080/api/runner/story',
         data: $storyid
       }).
       success(function(data){
         $scope.getStoryResponse(data, $storyid);
       });
-    }
+    };
 
     $scope.getStoryResponse = function($id, $storyid){
       $http({
         method: 'GET',
-        url: "http://192.168.42.44:8080/api/status/"+$id,
+        url: 'http://192.168.42.65:8080/api/status/'+$id,
       }).
       success(function(data){
         for(var i =0; i<$scope.stories.length; i++){
-          if($scope.stories[i].id == $storyid){
+          if($scope.stories[i].id === $storyid){
             $scope.stories[i].status = data.status; 
           }
         }
